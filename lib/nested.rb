6f42785bc803203cc463abb7c7bb8d854a9ca141
jzimmek <jan.zimmek@web.de>
2013-11-20 14:42:54 +0100

    remove delegate from core

--- lib/nested.rb
+++ lib/nested.rb
@@ -137,16 +137,6 @@ module Nested
       child_resource(name, true, false, init_block, &block)
     end
 
-    def delegate(name, &block)
-      singleton(name) do
-        resource = self
-        init do
-          instance_variable_set("@#{resource.instance_variable_name}", instance_variable_get("@#{resource.parent.instance_variable_name}"))
-        end
-        instance_eval(&block)
-      end
-    end
-
     def many(name, init_block=nil, &block)
       raise ManyInManyError.new "do not nest many in many" if collection?
       child_resource(name, false, true, init_block, &block)
